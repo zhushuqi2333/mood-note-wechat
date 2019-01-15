79b8f6462363fabbae36b257804b3d10bb7d1bec
柴玮 <793262255@qq.com>
2019-01-15 18:30:03 +0800

    修改登录页

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,22 +3,15 @@ const app = getApp()
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    isLogin: true,
+    avatarUrl: '',
+    userInfo: ''
   },
 
   onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
-
     // 获取用户信息
+    var that = this
+    that.onGetOpenid()
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
@@ -36,17 +29,7 @@ Page({
     })
   },
 
-  onGetUserInfo: function(e) {
-    if (!this.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
-  },
-
-  onGetOpenid: function() {
+  onGetOpenid: function () {
     // 调用云函数
     wx.cloud.callFunction({
       name: 'login',
@@ -55,7 +38,7 @@ Page({
         console.log('[云函数] [login] user openid: ', res.result.openid)
         app.globalData.openid = res.result.openid
         wx.navigateTo({
-          url: '../userConsole/userConsole',
+          url: '../home/index',
         })
       },
       fail: err => {
@@ -66,6 +49,18 @@ Page({
       }
     })
   },
+
+  onGetUserInfo: function(e) {
+    if (!this.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+  
 
   test: function () {
     console.log('进入云函数')
